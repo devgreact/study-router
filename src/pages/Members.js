e35f8c1e7ea90669eee48cad1d4ca7388bed8126
= <=>
2023-06-12 16:49:22 +0900

    feat : React Router 적용 및 react-youtube, fontAwsome 적용

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 /*
   리액트 컴포넌트에서 이미지 소스 불러오는 법
@@ -13,6 +14,21 @@ import React from "react";
   2.2. public 폴더 절대경로를 구해서 직접 출력
 */
 const Members = ({ members }) => {
+  // URI 경로를 이동할 수 있는 Hook
+  const navigate = useNavigate();
+  // 사용법 :  navigate(경로, 옵션)
+  const goHome = () => {
+    // navigate 를 이용해서 정보를 전달하고 싶다.
+    // state 옵션에 객체를 정의해서 전달해 준다.
+    // navigate("/", { state: { from: "/Members", age: 30 } });
+    navigate("/");
+  };
+
+  // location의 state 활용하기
+  // const showInfo = (_who, _img) => {
+  //   navigate("/about", { state: { singer: _who, photo: _img } });
+  // };
+
   const path = process.env.PUBLIC_URL;
   const imgStyle = {
     width: 90,
@@ -20,6 +36,7 @@ const Members = ({ members }) => {
   };
   const list = members.map((item, index) => {
     return (
+      // JSX 에서는 반복문 사용시 key = unique id 가 있어야 해요.
       <div key={index} className="col-6 col-md-4 col-lg-3">
         <img
           src={`${path}/${item.photo}`}
@@ -40,6 +57,10 @@ const Members = ({ members }) => {
       <div className="container">
         <div className="row">{list}</div>
       </div>
+
+      <button className="btn btn-secondary" onClick={goHome}>
+        Home
+      </button>
     </div>
   );
 };
